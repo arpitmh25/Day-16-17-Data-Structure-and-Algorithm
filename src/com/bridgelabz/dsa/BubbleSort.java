9c38749a7e5f5bb4f46c32ee0117b2724a7f8b64
Arpit <arpitmh23@gmail.com>
2022-01-05 21:18:09 +0530

    Adding Problem statement

---
+++ src/com/bridgelabz/dsa/BubbleSort.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.dsa;
+
+public class BubbleSort {
+
+    public void bubbleSort(String[] array) {
+        String temp;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BubbleSort sort = new BubbleSort();
+        String sentence = "hi,how,are,you";
+        String array[] = sentence.toLowerCase().split(",");
+        sort.bubbleSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
